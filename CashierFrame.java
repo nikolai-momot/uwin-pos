82e567bd9cdc8411c3fa82a5869ede53b37d6f6e
hsleiman1 <hsleiman1@hotmail.com>
2014-10-19 22:39:40 -0400

    Create CashierFrame.java
    sub-class of mainFrame.java

---
+++ CashierFrame.java
@@ -0,0 +1,18 @@
+package Project.java;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.event.ListSelectionEvent;
+
+public class CashierFrame extends mainFrame {
+	
+	public CashierFrame(String title){
+		super(title);
+	}
+	
+	public void actionPerformed(ActionEvent arg0) {
+	}
+
+	public void valueChanged(ListSelectionEvent e) {
+	}
+}
