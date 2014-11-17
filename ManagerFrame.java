abfd84d4e9a01006ce7a8b4b63f57c004d83eeb7
hsleiman1 <hsleiman1@hotmail.com>
2014-11-16 23:45:42 -0500

    Update ManagerFrame.java

--- ManagerFrame.java
+++ ManagerFrame.java
@@ -4,6 +4,7 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -17,12 +18,24 @@ public class ManagerFrame extends MainFrame  {
 		super(title);
 		GridBagConstraints c = new GridBagConstraints();
 		addItem = new JButton("Add item");
+		addItem.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+			
+		});
 		c.insets = new Insets(40, -350, 0, 0); //specifies the bottom distance
 		c.gridx = 0;
 		c.gridy = 4;
 		Gpanel.add(addItem, c);
 		
 		removeItem = new JButton("Remove item");
+		removeItem.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+			
+		});
 		c.insets = new Insets(40, -650, 0, 0); //specifies the bottom distance
 		c.gridx = 1;
 		c.gridy = 4;
@@ -30,9 +43,14 @@ public class ManagerFrame extends MainFrame  {
 		
 	}
 	
-	public void actionPerformed(ActionEvent arg0) {
-	}
 	
 	public void valueChanged(ListSelectionEvent arg0) {
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
