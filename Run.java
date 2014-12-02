91dc07f5c494187c8ad1bba05f28fb889e20e681
TheRyaz <ryan.lawson437@gmail.com>
2014-12-01 21:09:37 -0500

    Uploading Executable Version

--- Run.java
+++ Run.java
@@ -4,7 +4,5 @@ public class Run {
 	public static void main (String[] args){
 		Login frame = new Login("Login");
 		frame.setVisible(true);
-		/*ManagerFrame frame = new ManagerFrame("Manager frame");
-		frame.setVisible(true);*/
 	}
 }
